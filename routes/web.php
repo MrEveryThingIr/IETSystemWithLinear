f99f211faf6388d266f3d619490fdc2cd8757d60
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-08 21:32:47 +0330

    fix: retain email verification route import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\GroupInvitationController;
 use App\Livewire\Actors\Create;
 use App\Livewire\Actors\Edit;
