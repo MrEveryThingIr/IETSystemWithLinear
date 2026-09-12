89637e55381710f63b78a3b760b94e9fb44aa5dd
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-12 18:00:17 +0330

    style(spaces): document message factory definition

--- database/factories/GroupSpaceMessageFactory.php
+++ database/factories/GroupSpaceMessageFactory.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class GroupSpaceMessageFactory extends Factory
 {
+    /** @return array<string, mixed> */
     public function definition(): array
     {
         return [
