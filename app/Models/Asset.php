6c8244df9b569f2ccdce6aa647af3b0416bb635f
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-21 19:05:15 +0330

    fix(profile): type Profile Asset relation correctly

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 use LogicException;
 
@@ -180,7 +181,7 @@ class Asset extends Model
     }
 
     /** @return HasMany<ActorProfileImage, $this> */
-    public function profileImages(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function profileImages(): HasMany
     {
         return $this->hasMany(ActorProfileImage::class);
     }
