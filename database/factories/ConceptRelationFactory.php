a5ad5a37286949dc3ec746277c6661efa339d125
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-21 17:13:24 +0330

    test(concepts): add Concept relation factory

---
+++ database/factories/ConceptRelationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\ConceptAssertionSource;
+use App\Models\Actor;
+use App\Models\Concept;
+use App\Models\ConceptRelation;
+use App\Models\ConceptRelationType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends Factory<ConceptRelation> */
+class ConceptRelationFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'from_concept_id' => Concept::factory(),
+            'relation_type_id' => fn (): int => ConceptRelationType::query()
+                ->where('key', 'related_to')
+                ->valueOrFail('id'),
+            'to_concept_id' => Concept::factory(),
+            'source' => ConceptAssertionSource::Manual,
+            'created_by_actor_id' => Actor::factory(),
+            'metadata' => [],
+        ];
+    }
+}
