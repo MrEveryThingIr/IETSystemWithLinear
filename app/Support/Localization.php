0e74bd2c7247919b15d8c94ed3236db79be85bdd
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-21 21:49:31 +0330

    feat(temporal): expose locale temporal metadata

--- app/Support/Localization.php
+++ app/Support/Localization.php
@@ -4,7 +4,16 @@ namespace App\Support;
 
 class Localization
 {
-    /** @return array<string, array{name: string, native_name: string, direction: string}> */
+    /**
+     * @return array<string, array{
+     *     name: string,
+     *     native_name: string,
+     *     direction: string,
+     *     intl_locale?: string,
+     *     default_calendar?: string,
+     *     first_day_of_week?: int
+     * }>
+     */
     public static function supported(): array
     {
         $locales = config('localization.locales', []);
@@ -28,5 +37,28 @@ class Localization
         $direction = self::supported()[$locale ?? app()->getLocale()]['direction'] ?? 'ltr';
 
         return $direction === 'rtl' ? 'rtl' : 'ltr';
+    }
+
+    public static function intlLocale(?string $locale = null): string
+    {
+        $code = $locale ?? app()->getLocale();
+        $intl = self::supported()[$code]['intl_locale'] ?? str_replace('_', '-', $code);
+
+        return is_string($intl) && $intl !== '' ? $intl : 'en';
+    }
+
+    public static function defaultCalendar(?string $locale = null): string
+    {
+        $calendar = self::supported()[$locale ?? app()->getLocale()]['default_calendar'] ?? 'gregory';
+
+        return is_string($calendar) && $calendar !== '' ? $calendar : 'gregory';
+    }
+
+    public static function firstDayOfWeek(?string $locale = null): int
+    {
+        $day = self::supported()[$locale ?? app()->getLocale()]['first_day_of_week'] ?? 1;
+        $day = is_numeric($day) ? (int) $day : 1;
+
+        return min(7, max(1, $day));
     }
 }
