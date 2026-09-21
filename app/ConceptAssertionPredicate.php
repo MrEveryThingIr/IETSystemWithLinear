44f5e5606a250a22148e68b65e9d2e5c138b8d7e
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-21 16:35:44 +0330

    feat(concepts): add assertion predicate registry

---
+++ app/ConceptAssertionPredicate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+enum ConceptAssertionPredicate: string
+{
+    case HasSkill = 'has_skill';
+    case WantsToLearn = 'wants_to_learn';
+    case InterestedIn = 'interested_in';
+    case Needs = 'needs';
+    case About = 'about';
+    case Teaches = 'teaches';
+    case FocusesOn = 'focuses_on';
+}
