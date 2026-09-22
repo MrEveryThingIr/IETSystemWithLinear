62e4c424721c99791b4a3ad4dae920c10d9102a1
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-22 10:38:43 +0330

    ui(profile): use on-demand plus actions

--- resources/views/livewire/profile/intents.blade.php
+++ resources/views/livewire/profile/intents.blade.php
@@ -11,7 +11,7 @@
             <p class="mt-1 text-sm text-zinc-500">{{ __('ui.profile.intents.help') }}</p>
         </div>
         @unless ($editorOpen)
-            <flux:button wire:click="openCreate" size="sm" variant="ghost" class="w-full shrink-0 sm:w-auto">
+            <flux:button wire:click="openCreate" size="sm" variant="ghost" icon="plus" class="w-full shrink-0 sm:w-auto">
                 {{ __('ui.profile.intents.add') }}
             </flux:button>
         @endunless
