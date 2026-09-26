55cc9107a0e8174b3897e271084cfb7d80e73bab
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-26 09:18:33 +0330

    feat(temporal): add profile-aware temporal display components

---
+++ resources/views/components/app/local-date.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'value',
+    'showEquivalent' => true,
+])
+
+@php
+    $user = request()->user();
+    $date = $value instanceof \DateTimeInterface ? $value->format('Y-m-d') : (string) $value;
+    $calendar = \App\Support\TemporalPreferences::calendarFor($user)->value;
+    $locale = \App\Support\Localization::intlLocale($user?->locale);
+@endphp
+
+<span
+    {{ $attributes->merge(['class' => 'inline-flex flex-col']) }}
+    data-profile-date="{{ $date }}"
+    data-locale="{{ $locale }}"
+    data-calendar="{{ $calendar }}"
+    data-show-equivalent="{{ $showEquivalent ? 'true' : 'false' }}"
+>
+    <span data-temporal-primary>{{ $date }}</span>
+    @if ($showEquivalent)
+        <span data-temporal-equivalent class="text-[0.72rem] font-normal text-zinc-500" hidden></span>
+    @endif
+</span>
