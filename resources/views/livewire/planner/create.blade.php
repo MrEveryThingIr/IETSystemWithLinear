c6123ac312ea53d68511689adba96f7b894d347b
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-26 10:12:32 +0330

    planner: use localized calendar inputs and explain execution window

--- resources/views/livewire/planner/create.blade.php
+++ resources/views/livewire/planner/create.blade.php
@@ -43,13 +43,13 @@
                     @endforeach
                 </flux:select>
 
-                <flux:input wire:model="startsOn" type="date" :label="__('planner.create.starts_on')" />
-                <flux:input wire:model="startTime" type="time" :label="__('planner.create.start_time')" />
+                <x-app.calendar-date-input model="startsOn" :label="__('planner.create.starts_on')" />
+                <flux:input wire:model="startTime" type="time" step="60" :label="__('planner.create.start_time')" />
                 <flux:input wire:model="durationMinutes" type="number" min="1" max="10080" :label="__('planner.create.duration')" />
                 <flux:input wire:model="interval" type="number" min="1" max="365" :label="__('planner.create.interval')" />
 
                 @if (in_array($frequency, ['daily', 'weekly'], true))
-                    <flux:input wire:model="endsOn" type="date" :label="__('planner.create.ends_on')" />
+                    <x-app.calendar-date-input model="endsOn" :label="__('planner.create.ends_on')" />
                     <flux:input wire:model="occurrenceLimit" type="number" min="1" max="10000" :label="__('planner.create.occurrence_limit')" />
                 @endif
             </div>
@@ -76,9 +76,20 @@
             @endif
 
             <div class="grid gap-4 sm:grid-cols-2">
-                <flux:input wire:model="windowBeforeMinutes" type="number" min="0" max="10080" :label="__('planner.create.early_window')" />
-                <flux:input wire:model="windowAfterMinutes" type="number" min="0" max="10080" :label="__('planner.create.late_window')" />
+                <div>
+                    <flux:input wire:model="windowBeforeMinutes" type="number" min="0" max="10080" :label="__('planner.create.early_window')" />
+                    <p class="mt-1 text-xs text-zinc-500">{{ __('planner.create.early_window_help') }}</p>
+                </div>
+                <div>
+                    <flux:input wire:model="windowAfterMinutes" type="number" min="0" max="10080" :label="__('planner.create.late_window')" />
+                    <p class="mt-1 text-xs text-zinc-500">{{ __('planner.create.late_window_help') }}</p>
+                </div>
             </div>
+
+            <flux:callout>
+                <div class="font-medium">{{ __('planner.create.execution_title') }}</div>
+                <div class="mt-1 text-sm">{{ __('planner.create.execution_help') }}</div>
+            </flux:callout>
 
             <div>
                 <flux:input wire:model="reminderOffsets" :label="__('planner.create.reminders')" />
