71ff6b80293199a125ad125b888fb24473d0e3d7
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-26 09:18:36 +0330

    feat(temporal): add profile-aware temporal display components

---
+++ resources/views/components/app/local-datetime.blade.php
@@ -0,0 +1,31 @@
+@props([
+    'value',
+    'showEquivalent' => true,
+    'seconds' => false,
+])
+
+@php
+    $user = request()->user();
+    $timezone = \App\Support\TemporalPreferences::timezoneFor($user);
+    $calendar = \App\Support\TemporalPreferences::calendarFor($user)->value;
+    $locale = \App\Support\Localization::intlLocale($user?->locale);
+    $instant = $value instanceof \DateTimeInterface
+        ? \Carbon\CarbonImmutable::instance($value)->toIso8601String()
+        : (string) $value;
+@endphp
+
+<time
+    {{ $attributes->merge(['class' => 'inline-flex flex-col']) }}
+    datetime="{{ $instant }}"
+    data-profile-datetime="{{ $instant }}"
+    data-locale="{{ $locale }}"
+    data-calendar="{{ $calendar }}"
+    data-timezone="{{ $timezone }}"
+    data-show-equivalent="{{ $showEquivalent ? 'true' : 'false' }}"
+    data-seconds="{{ $seconds ? 'true' : 'false' }}"
+>
+    <span data-temporal-primary>{{ $instant }}</span>
+    @if ($showEquivalent)
+        <span data-temporal-equivalent class="text-[0.72rem] font-normal text-zinc-500" hidden></span>
+    @endif
+</time>
