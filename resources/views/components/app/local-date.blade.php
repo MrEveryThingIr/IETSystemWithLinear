5be7ce3194c8e7a821ab96235539616de8c28b4d
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-26 09:51:11 +0330

    fix(temporal): server-render localized date component

--- resources/views/components/app/local-date.blade.php
+++ resources/views/components/app/local-date.blade.php
@@ -6,8 +6,16 @@
 @php
     $user = request()->user();
     $date = $value instanceof \DateTimeInterface ? $value->format('Y-m-d') : (string) $value;
-    $calendar = \App\Support\TemporalPreferences::calendarFor($user)->value;
+    $calendarSystem = \App\Support\TemporalPreferences::calendarFor($user);
+    $calendar = $calendarSystem->value;
     $locale = \App\Support\Localization::intlLocale($user?->locale);
+    $dateValue = $value instanceof \DateTimeInterface
+        ? \Carbon\CarbonImmutable::instance($value)
+        : \Carbon\CarbonImmutable::parse($date, 'UTC');
+    $primaryLabel = \App\Support\TemporalCalendar::dateLabel($dateValue, $user, 'UTC', $calendarSystem);
+    $equivalentLabel = $calendarSystem === \App\CalendarSystem::Gregorian
+        ? ''
+        : \App\Support\TemporalCalendar::dateLabel($dateValue, $user, 'UTC', \App\CalendarSystem::Gregorian);
 @endphp
 
 <span
@@ -18,8 +26,8 @@
     data-show-equivalent="{{ $showEquivalent ? 'true' : 'false' }}"
     data-equivalent-label="{{ __('ui.profile.temporal.gregorian_equivalent') }}"
 >
-    <span data-temporal-primary>{{ $date }}</span>
+    <span data-temporal-primary>{{ $primaryLabel }}</span>
     @if ($showEquivalent)
-        <span data-temporal-equivalent class="text-[0.72rem] font-normal text-zinc-500" hidden></span>
+        <span data-temporal-equivalent class="text-[0.72rem] font-normal text-zinc-500" @if (! $showEquivalent || $equivalentLabel === '') hidden @endif>{{ $equivalentLabel === '' ? '' : __('ui.profile.temporal.gregorian_equivalent').' · '.$equivalentLabel }}</span>
     @endif
 </span>
