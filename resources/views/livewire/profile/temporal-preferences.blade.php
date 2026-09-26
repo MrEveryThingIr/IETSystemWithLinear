cde95a8e2db2ba6f689b56227ed6d84046fa7cf3
MrEveryThingIr <school.mreverything@gmail.com>
2026-09-26 09:49:15 +0330

    fix(temporal): label profile Gregorian equivalent preview

--- resources/views/livewire/profile/temporal-preferences.blade.php
+++ resources/views/livewire/profile/temporal-preferences.blade.php
@@ -20,6 +20,7 @@
             data-locale="{{ $intlLocale }}"
             data-calendar="{{ $resolvedCalendar }}"
             data-timezone="{{ $timezone }}"
+            data-equivalent-label="{{ __('ui.profile.temporal.gregorian_equivalent') }}"
             >
                 <span class="block text-xs font-medium uppercase tracking-wide text-zinc-500">{{ __('ui.profile.temporal.preview') }}</span>
                 <span class="mt-1 block break-words font-medium" data-temporal-preview-value>{{ __('ui.profile.temporal.preview_loading') }}</span>
